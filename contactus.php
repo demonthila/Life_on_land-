0c2d3627e48b61cb6742d343ed706c83a1afdb51
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 21:32:27 +0530

    php if statement

--- contactus.php
+++ contactus.php
@@ -17,4 +17,16 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])){
 
             
                     $isSaved = mysqli_query($connection, $query);
+
+
+                    if ($isSaved) {
+                        header("Location: ../index.php?msg=Register Successful !");
+                        die();
+                    } else {
+                        header("Location: ../index.php?msg=Error,Register Unsuccessful !");
+                        die();
+                    }
+        
+        }
+
 ?>
