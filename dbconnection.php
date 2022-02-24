4f6936cdc37f4d195a08820ca01bf03a0803e787
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 22:42:18 +0530

    port identification

--- dbconnection.php
+++ dbconnection.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $username = "root";
 $passwd = "ass34";
 $dbname = "opd_channeling_db";
-$port = "3307";
+$port = "3307";//port identification
 
 
 $connection = new mysqli($host, $username, $passwd, $dbname, $port);
