ede7ed85318467d69ccc94023ef2c9fe67815dfa
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 21:27:43 +0530

    php  query column

--- contactus.php
+++ contactus.php
@@ -12,4 +12,7 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])){
     $email = $_POST["email"];
     $message = $_POST["message"]
 
+    $query = "INSERT INTO tableName (columnName1, columnName2, columnName3) "
+                    . "VALUES ('" . $name . "','" . $email . "','" . $message . "')";
+
 ?>
