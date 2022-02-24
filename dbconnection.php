898a5d5b82433281e7593294639d2b581440361f
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 21:45:19 +0530

    db connection checking

--- dbconnection.php
+++ dbconnection.php
@@ -9,5 +9,11 @@ $port = "3307";
 
 $connection = new mysqli($host, $username, $passwd, $dbname, $port);
 
+if ($connection->connect_error) {
+    echo 'Error !, Not Connected' 
+    . $connection->connect_error;
+} else {
+        echo "conected Successfully";
+}
 
 ?>
