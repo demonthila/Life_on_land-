a0ac8d9ba24c5bbb13b654992a47e232d54dcf3a
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 22:24:02 +0530

    adding columns

--- contactus.php
+++ contactus.php
@@ -12,7 +12,7 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])){
     $email = $_POST["email"];
     $message = $_POST["message"]
 
-    $query = "INSERT INTO tableName (columnName1, columnName2, columnName3) "
+    $query = "INSERT INTO tableName (name, email, message) "
                     . "VALUES ('" . $name . "','" . $email . "','" . $message . "')";
 
             
