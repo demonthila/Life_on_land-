ee248a49b1e49d6659a260e66b49f9c23379b823
wwnsdsilva <100225743+wwnsdsilva@users.noreply.github.com>
2022-02-24 23:01:44 +0530

    db connection checking part readded

--- dbconnection.php
+++ dbconnection.php
@@ -9,6 +9,11 @@ $port = "3307";//port identification
 
 $connection = new mysqli($host, $username, $passwd, $dbname, $port);
 
-
+//if ($connection->connect_error) {
+    //echo 'Error !, Not Connected' 
+    //. $connection->connect_error;
+} //else {
+    // echo "conected Successfully";
+}
 
 ?>
